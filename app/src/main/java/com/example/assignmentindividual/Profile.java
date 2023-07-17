a7c01ad79b88e1dc74a215cb47152152236bf670
unknown <aisahyani>
2023-07-17 15:40:22 +0800

    first commit

---
+++ app/src/main/java/com/example/assignmentindividual/Profile.java
@@ -0,0 +1,70 @@
+package com.example.assignmentindividual;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Profile extends AppCompatActivity {
+
+    TextView aisah,uitm,copy;
+    Button sourcecode;
+    ImageView ecah;
+    Dialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        aisah = (TextView) findViewById(R.id.aisah);
+        uitm = (TextView) findViewById(R.id.uitm);
+        copy = (TextView) findViewById(R.id.copy);
+        ecah = (ImageView) findViewById(R.id.ecah);
+
+        dialog= new Dialog(this);
+
+        sourcecode = (Button) findViewById(R.id.sourcecode);
+
+        sourcecode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/fikrinazeef/Assignment"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.bmi) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                return(true);
+        }
+
+        return (super.onOptionsItemSelected(item));
+    }
+
+
+}
